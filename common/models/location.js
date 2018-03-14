86d9a747eb2a597705ae264c98b628e339294615
Ajith Vasudevan <ajith.vasudevan@gmail.com>
2018-03-14 23:13:15 +0530

    Added map.html

--- common/models/location.js
+++ common/models/location.js
@@ -1,6 +1,5 @@
 module.exports = function locFn(Location) {
 	Location.observe('before save', function locAfterSaveFn(ctx, next) {
-                console.log(Object.keys(ctx.Model));
 		if(ctx.instance && ctx.instance.loc) {
 			coords = (ctx.instance.loc).split(",");
 			ctx.instance.latitude = Number(coords[0]);
