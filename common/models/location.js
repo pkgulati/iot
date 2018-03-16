4433bb61d5bfb6fbf73c876f276b1c91d4367f9c
ajith <ajith_v@edgeverve.com>
2018-03-16 14:05:49 +0530

    Fixes

--- common/models/location.js
+++ common/models/location.js
@@ -12,9 +12,57 @@ module.exports = function locFn(Location) {
 
 	Location.observe('after save', function locAfterSaveFn(ctx, next) {
 		if(ctx.instance) {
-			var socket = Location.app.skt;
-			socket.emit('message1', ctx.instance);
+			console.log("Location after save: sending data", JSON.stringify(ctx.instance));
+			var sockets = Location.app.skts;
+			sockets.forEach(function(socket) {
+				socket.emit('message1', ctx.instance);
+			});
+			
 		}
 		next();
 	});
+
+	Location.remoteMethod('loc', {
+		description: 'Sets new location from a Google Maps URL',
+		accessType: 'READ',
+		accepts: [{
+		  arg: 'url',
+		  type: 'string',
+		  description: 'url',
+		  http: {
+			source: 'query'
+		  }
+		}
+		],
+		http: {
+		  verb: 'GET',
+		  path: '/loc'
+		},
+		returns: {
+		  type: 'object',
+		  root: true
+		}
+	  });
+
+
+	  Location.loc = function loc(url, options, cb) {
+		var data = {};
+		if (!cb && typeof options === 'function') {
+		  cb = options;
+		  options = {};
+		}
+		var firstIndexOfComma = url.indexOf(",", url.indexOf("@"));
+		var secondIndexOfComma = url.indexOf(",", 2 + firstIndexOfComma);
+		var latlng = url.substring(1 + url.indexOf("@"), secondIndexOfComma);
+		var name = url.substring(0, url.indexOf("/"));
+		if(name.startsWith("http")) name = "UNNAMED";
+		var lat = Number(latlng.substring(0, latlng.indexOf(',')));
+		var lng = Number(latlng.substring(1 + latlng.indexOf(',')));
+		var tm = new Date().getTime()/1000;
+		var type = "URL";
+		var data = {"time": tm, "latitude": lat, "longitude": lng, "name": name, "type": type};
+		Location.upsert(data, options.ctx, function upd(err, data) {
+			cb(err, data);
+		  });
+	  };
 }
--- server/boot/wsocket.js
+++ server/boot/wsocket.js
@@ -1,12 +1,13 @@
-
 module.exports = function WSocket(app, cb) {
+    var sockets = [];
     var io = require('socket.io')(app.server);
 
     console.log("Boot function WSocket");
     io.on('connection', function(socket){
         console.log('a user connected');
         socket.emit('message1', {"Hello": "World" });
-        app.skt = socket;
-      });
-  cb();
+        sockets.push(socket);
+    });
+    app.skts = sockets;
+    cb();
 };
