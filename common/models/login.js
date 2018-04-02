8ee559e7f33035cacdc53aa24730dfec318a1c5a
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-04-02 15:19:24 +0530

    Corrected login method

--- common/models/login.js
+++ common/models/login.js
@@ -2,56 +2,22 @@ var loopback = require("loopback");
 
 module.exports = function(Login) {
   Login.login = function(data, options, cb) {
-   
-    if (!data.email) {
-      var err2 = new Error("email is required to login");
+    if (!data.user) {
+      var err2 = new Error("username is required to login");
       err2.statusCode = 400;
-      err2.code = "USERNAME_EMAIL_REQUIRED";
+      err2.code = "USERNAME_REQUIRED";
       err2.retriable = false;
       cb(err2, null);
       return false;
     }
-    var UserModel = loopback.getModelByType("BaseUser");
-    var filter = {
-        where : {
-            email: data.email
-        }
+    var credentials = {
+      username : data.user,
+      password : data.password
     };
     options.ctx = options.ctx || {};
     options.ctx.tenantId = "defult";
-    UserModel.findOne(filter, options, function findOneCb(err, user) {
-      if (user) {
-        UserModel.login(data, options, cb);
-      } else {
-        var nameParts = data.email.split("@");
-        if (nameParts.length != 2) {
-          var err2 = new Error("email is required to login");
-          err2.statusCode = 400;
-          err2.code = "USERNAME_EMAIL_REQUIRED";
-          err2.retriable = false;
-          cb(err2, null);
-          return false;
-        }
-        var userData = {
-          username: data.email,
-          email: data.email,
-          emailVerified: true,
-          password: data.password
-        };
-        UserModel.create(userData, options, function(err, newUser) {
-          if (err) {
-            var defaultError = new Error('Could not automatically sign in');
-            defaultError.statusCode = 401;
-            defaultError.code = 'LOGIN_FAILED';
-            defaultError.retriable = false;
-            err.additionalMessage = "Could not automatically sign in";
-            return cb(err, null);
-          }
-          console.log("newUser created ", newUser.id, newUser.username);
-          UserModel.login(data, options, cb);
-        });
-      }
-    });
+    var UserModel = loopback.getModelByType('BaseUser');
+    UserModel.login(credentials, options, cb);
   };
 
   Login.remoteMethod("login", {
