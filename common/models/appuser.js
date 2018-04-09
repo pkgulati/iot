fb64dbb8dbf735bf836d4d1fc753767fa7666f9e
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-04-09 10:30:29 +0530

    Added dashboard method and contact Group

--- common/models/appuser.js
+++ common/models/appuser.js
@@ -2,43 +2,45 @@ var loopback = require("loopback");
 var async = require("async");
 
 module.exports = function(UserModel) {
-  UserModel.contactInfo = function(users, options, cb) {
-    var response = [];
-    if (!options.ctx.userId) {
-        return cb(null, response);
-    }
-    var filter = {
-        where : {
-            userId : options.ctx.userId
-        }
-    };
-
-    var ContactModel = loopback.getModelByType("Contact");
-    var UserInfoModel = loopback.getModelByType("UserInfo");
-    var TeamMemberModel = loopback.getModelByType("TeamMember");
-    
-    ContactModel.find(filter, options, function(err, list) {
-        async.mapSeries(list, function(contact, done) {
-            var id = contact.contactUserId || contact.contactId;
-            UserInfoModel.findById(id, options, function(err, dbinfo){
-                done(null, dbinfo);
-            });
-        }, function(err, results) {
-            cb(err, results);
+  UserModel.prototype.dashboard = function(options, cb) {
+    console.log('this user Id ', this.id);
+    var userId = this.id;
+    async.parallel ({
+      contacts : function(done) {
+        var ContactModel = loopback.getModelByType("Contact");
+        var filter = {where:{ownerUserId : userId}};
+        ContactModel.find(filter, options, function(err, list) {
+          done(err, list);
         });
-    });
-
-    var filter2 = {
-      where : {
-          userId : options.ctx.userId
+      },
+      groups : function(done) {
+        var ContactGroup = loopback.getModelByType("ContactGroup");
+        var filter = {where:{ownerUserId : userId}};
+        ContactGroup.find(filter, options, function(err, list) {
+          done(err, list);
+        });
       }
-    };
-
-    TeamMemberModel.find(filter2, options, function(err, list) {
-    });
-
-
-
+      },
+      function(err, results) {
+        cb(err, results);
+      }
+    );
 };
+
+UserModel.remoteMethod("dashboard", {
+  description: "fetch contacts and groups and their information",
+  accessType: "READ",
+  isStatic : false,
+  accepts: [
+  ],
+  http: {
+    verb: "GET",
+    path: "/dashboard"
+  },
+  returns: {
+    type: "object",
+    root: true
+  }
+});
 
 };
