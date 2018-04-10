d0421eef35fac599d65a021c4da7d187f7388b36
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-04-10 11:19:11 +0530

    Added FCM for location change

---
+++ common/models/user-info.js
@@ -0,0 +1,35 @@
+var loopback = require("loopback");
+var async = require("async");
+
+module.exports = function(UserInfo) {
+
+    UserInfo.observe('after save', function(ctx, next) {
+        next();
+        if (ctx.instance) {
+            var info = ctx.instance;
+            var models = UserInfo.app.models;
+            models.Contact.find({where:{contactUserId:info.userId}}, ctx.options, function(err, contacts){
+                var userIds = [];
+                contacts.forEach(function(contact) {
+                    userIds.push(contact.ownerUserId.toString());
+                });
+                models.AppUser.find({where:{id:{inq:userIds}}}, ctx.options, function(err, users){
+                    users.forEach(function(user){
+                        if (user.deviceToken) {
+                            var message = {
+                                token : user.deviceToken,
+                                data : {
+                                    type : "UserInfoChanged",
+                                    info : info.toJson()
+                                }
+                            };
+                            models.FCM.push(message, ctx.options, function(){ 
+                            });
+                        }
+                    });
+                });
+            });
+        }
+    });
+ 
+};
