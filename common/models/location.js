c5880db0eea32885e82834434bbf0c8df368cbda
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-04-13 15:30:19 +0530

    Changed accuracy limit

--- common/models/location.js
+++ common/models/location.js
@@ -30,7 +30,7 @@ module.exports = function locFn(Location) {
 	Location.observe('after save', function(ctx, next) {
 		console.log('posting location ', ctx.options.ctx.username );
 		if(ctx.instance && ctx.options.ctx && ctx.options.ctx.userId) {
-				if (ctx.instance.accuracy && ctx.instance.accuracy < 40) {
+				if (ctx.instance.accuracy && ctx.instance.accuracy < 700) {
 				console.log('time ', ctx.instance.time);
 				var UserInfoModel = loopback.getModelByType("UserInfo");
 				var filter = {where : {id : ctx.options.ctx.userId}};
