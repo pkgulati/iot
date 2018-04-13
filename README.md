882dcacd851a620b7d2c3283459c9a238c69f6a6
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-04-13 09:01:22 +0530

    test

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # iot
 Application to learn and share IOT stuff
+
+# Android App
+Data Refresh : Upon FCM receive 
+
+Sending Location :
+    User Triggered      :
+    
+    Periodic            :
+   
+    On Demand           : 
+            Override previous demands
+            Collect demands
+
+Calling startService() multiple times does not result in starting multiple service.
+
+
+
+
--- server/test2.js
+++ server/test2.js
@@ -1,4 +1,17 @@
 var contactUserIds = ["us1", "us2"];
+var loc = {
+    "name": "ajith",
+    "latitude": 12.8497239,
+    "longitude": 77.6658435,
+    "accuracy": 27.093000411987305,
+    "id": "5acd8bce50c84ff6503f97a5",
+    "time": 1523420109,
+    "type": "app88",
+    "formattedTime": "2018-04-11T09:45:09+0530",
+    "username": "ajith"
+  };
+var d = new Date(loc.formattedTime);
+console.log(d);
 contactUserIds.forEach(function(item){
     console.log('contactUserId ', item);
 });
