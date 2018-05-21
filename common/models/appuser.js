3929eedcd44570da4e27407f26f75d628c27cc90
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-05-21 22:17:49 +0530

    Added one more api to know next job interval

--- common/models/appuser.js
+++ common/models/appuser.js
@@ -2,24 +2,47 @@ var loopback = require("loopback");
 var async = require("async");
 
 module.exports = function(UserModel) {
+  UserModel.prototype.nextjob = function(options, cb) {
+    var res = {
+      interval: 1000
+    };
+    cb(null, res);
+  };
+
+  UserModel.remoteMethod("nextjob", {
+    description: "fetch nextjob interval",
+    accessType: "READ",
+    isStatic: false,
+    accepts: [],
+    http: {
+      verb: "GET",
+      path: "/nextjob"
+    },
+    returns: {
+      type: "object",
+      root: true
+    }
+  });
+
   UserModel.prototype.data = function(options, cb) {
-    console.log('fetching user data for  ', this.username);
+    console.log("fetching user data for  ", this.username);
     var userId = this.id;
-    async.parallel ({
-      contacts : function(done) {
-        var ContactModel = loopback.getModelByType("Contact");
-        var filter = {where:{ownerUserId : userId}};
-        ContactModel.find(filter, options, function(err, list) {
-          done(err, list);
-        });
-      },
-      groups : function(done) {
-        var ContactGroup = loopback.getModelByType("ContactGroup");
-        var filter = {where:{ownerUserId : userId}};
-        ContactGroup.find(filter, options, function(err, list) {
-          done(err, list);
-        });
-      }
+    async.parallel(
+      {
+        contacts: function(done) {
+          var ContactModel = loopback.getModelByType("Contact");
+          var filter = { where: { ownerUserId: userId } };
+          ContactModel.find(filter, options, function(err, list) {
+            done(err, list);
+          });
+        },
+        groups: function(done) {
+          var ContactGroup = loopback.getModelByType("ContactGroup");
+          var filter = { where: { ownerUserId: userId } };
+          ContactGroup.find(filter, options, function(err, list) {
+            done(err, list);
+          });
+        }
       },
       function(err, results) {
         var information = {};
@@ -27,42 +50,40 @@ module.exports = function(UserModel) {
           information[contact.contactUserId] = {};
         });
         results.groups.forEach(function(group) {
-          group.contactUserIds.forEach(function(userId){
+          group.contactUserIds.forEach(function(userId) {
             information[userId] = {};
           });
         });
-        var filter = {where : {id:{inq:Object.keys(information)}}};
+        var filter = { where: { id: { inq: Object.keys(information) } } };
         var InformationModel = loopback.getModelByType("UserInfo");
         InformationModel.find(filter, options, function(err, list) {
-          list.forEach(function(info){
+          list.forEach(function(info) {
             information[info.id] = info;
           });
           results.information = information;
           results.configuration = {
-            locationJobInterval : 2400000,
-            locationServiceStopTimer : 300000,
-            locationServiceRestartTimer : 40000
+            locationJobInterval: 2400000,
+            locationServiceStopTimer: 300000,
+            locationServiceRestartTimer: 40000
           };
-          cb(err, results);    
+          cb(err, results);
         });
       }
     );
-};
+  };
 
-UserModel.remoteMethod("data", {
-  description: "fetch contacts and groups and their information",
-  accessType: "READ",
-  isStatic : false,
-  accepts: [
-  ],
-  http: {
-    verb: "GET",
-    path: "/data"
-  },
-  returns: {
-    type: "object",
-    root: true
-  }
-});
-
+  UserModel.remoteMethod("data", {
+    description: "fetch contacts and groups and their information",
+    accessType: "READ",
+    isStatic: false,
+    accepts: [],
+    http: {
+      verb: "GET",
+      path: "/data"
+    },
+    returns: {
+      type: "object",
+      root: true
+    }
+  });
 };
