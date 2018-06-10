7482d2a1f998ff435f817a322190972382b8ab2e
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-06-10 22:45:45 +0530

    format

--- common/models/user-configuration.js
+++ common/models/user-configuration.js
@@ -1,19 +1,19 @@
 loopback = require("loopback");
 
 module.exports = function(UserConfiguration) {
-var tofcm = function (obj) {
-      Object.keys(obj).forEach(function(k) {
-        var val = obj[k];
-        switch (typeof val) {
-            case "number":
-                obj[k] = val.toString();
-            break;
-            case "boolean":
-                obj[k] = val.toString();
-            break;
-            case "object" : 
-                delete obj[k];
-        }
+  var tofcm = function(obj) {
+    Object.keys(obj).forEach(function(k) {
+      var val = obj[k];
+      switch (typeof val) {
+        case "number":
+          obj[k] = val.toString();
+          break;
+        case "boolean":
+          obj[k] = val.toString();
+          break;
+        case "object":
+          delete obj[k];
+      }
     });
   };
 
@@ -23,17 +23,16 @@ var tofcm = function (obj) {
       var models = UserConfiguration.app.models;
       models.AppUser.findById(userId, ctx.options, function(err, user) {
         if (user && user.deviceToken) {
-	
-var data = ctx.instance.toJSON();
+          var data = ctx.instance.toJSON();
           tofcm(data);
           data.type = "configure";
-	data = JSON.parse(JSON.stringify(data));
+          data = JSON.parse(JSON.stringify(data));
           var message = {
             token: user.deviceToken,
             data: data
           };
           models.FCM.push(message, ctx.options, function(err, res) {
-        		console.log("FCM configure response ", err, res);
+            console.log("FCM configure response ", err, res);
           });
         }
       });
--- common/models/user-info.js
+++ common/models/user-info.js
@@ -2,34 +2,37 @@ var loopback = require("loopback");
 var async = require("async");
 
 module.exports = function(UserInfo) {
-
-  UserInfo.statusCache = {
-  };
+  UserInfo.statusCache = {};
 
   UserInfo.OnlineContacts = {};
 
-
   UserInfo.observe("after save", function(ctx, next) {
+    
     next();
+
     if (ctx.instance) {
       // toJson ?
       UserInfo.statusCache[ctx.instance.id] = ctx.instance.toJSON;
       var info = ctx.instance;
       var models = UserInfo.app.models;
-        var ts = new Date();
-        ts = ts.getTime();
-        ts = ts - 30*60*1000;
-	var filter = {
-		where : {
-			contactUserId : ctx.instance.id,
-			lastViewedAt : { gt : ts }
-		}
-	};
-        models.Contact.find(filter, ctx.options, function(err, list) {
+      var ts = new Date();
+      ts = ts.getTime();
+      ts = ts - 30 * 60 * 1000;
+      var filter = {
+        where: {
+          contactUserId: ctx.instance.id,
+          lastViewedAt: { gt: ts }
+        }
+      };
+
+      models.Contact.find(filter, ctx.options, function(err, list) {
         list.forEach(function(contact) {
-          models.AppUser.findById(contact.ownerUserId, ctx.options, function(err, user) {
+          models.AppUser.findById(contact.ownerUserId, ctx.options, function(
+            err,
+            user
+          ) {
             if (user && user.deviceToken) {
-              console.log('sending to user ', user.username);
+              console.log("sending to user ", user.username);
               var message = {
                 token: user.deviceToken,
                 data: {
@@ -40,27 +43,27 @@ module.exports = function(UserInfo) {
                   longitude: info.longitude.toString(),
                   accuracy: info.accuracy.toString(),
                   lastLocationTime: info.lastLocationTime.toJSON(),
-                  provider : info.provider || 'x'
+                  provider: info.provider || "x"
                 }
               };
-            models.FCM.push(message, ctx.options, function(err, res) {});
-          }
+              models.FCM.push(message, ctx.options, function(err, res) {});
+            }
+          });
         });
       });
-    });
-  };
-});
+    }
+  });
 
-UserInfo.prototype.next = function(options, cb) {
-  var response = {
-    finishJob : true,
-    nextJobMinutes : 30,
-    startService : false
+  UserInfo.prototype.next = function(options, cb) {
+    var response = {
+      finishJob: true,
+      nextJobMinutes: 30,
+      startService: false
+    };
+    return cb(null, response);
   };
-  return cb(null, response);
-};
 
-UserInfo.remoteMethod("next", {
+  UserInfo.remoteMethod("next", {
     description: "get next action",
     accessType: "READ",
     isStatic: false,
@@ -74,6 +77,4 @@ UserInfo.remoteMethod("next", {
       root: true
     }
   });
-
 };
-
